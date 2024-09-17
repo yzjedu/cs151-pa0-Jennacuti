0b5b3791d4d97f27a2162f55e1f813fdca95fae4
Jenna and Caitlin <jmcuti@loyola.edu>
2024-09-17 17:36:54 -0400

    #Programmer : Jenna Cuti #Course : CS151, Dr Zee Yalew #Due 9/18/24 #Programming Assignment PA00 #Problem Statement : Convert Feet to Yards #Data In: User prompted to put number of feet #Data Out: How many yards per foot #credits : Zybooks and Class Examples

---
+++ main.py
@@ -0,0 +1,8 @@
+#prompt the user to enter a value in feet
+feet=float(input("Enter your feet: "))
+
+#convert feet to yards using the formula yards = feet / 3
+yards = feet / 3
+
+# output the result in yards
+print("The equivalent value in yards is:" , yards)
